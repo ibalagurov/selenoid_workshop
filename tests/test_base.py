471aba263a8af66b530c6fb3644714b0a26cebc9
Igor Balagurov <balagurovi@yandex.ru>
2020-02-16 11:27:31 +0300

    Add cross-browser example with parallel run

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,32 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-capabilities = {
-    "browserName": "chrome",
-    "version": "80.0",
-    "platform": "LINUX",
-    # "enableVNC": True,
-    # "enableVideo": True,
-}
+import pytest
 
 
-def test_test():
-    driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        desired_capabilities=capabilities
-    )
+@pytest.mark.parametrize(
+    "browser_name,version",
+    [
+        ("chrome", "80.0"),
+        ("chrome", "79.0"),
+        ("firefox", "71.0"),
+        ("firefox", "72.0"),
+        ("opera", "65.0"),
+        ("opera", "66.0"),
+    ],
+)
+def test_test(browser_name, version):
+    capabilities = {
+        "browserName": browser_name,
+        "version": version,
+        "platform": "LINUX",
+        # "enableVNC": True,
+        # "enableVideo": True,
+    }
+    driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", desired_capabilities=capabilities)
 
     try:
-        print('Session ID is: %s' % driver.session_id)
-        print('Opening the page...')
-        driver.get('http://duckduckgo.com/')
+        print("Session ID is: %s" % driver.session_id)
+        print("Opening the page...")
+        driver.get("http://duckduckgo.com/")
 
-        print('Typing search request...')
-        search_input = driver.find_element_by_css_selector(
-            "input[id='search_form_input_homepage']")
+        print("Typing search request...")
+        search_input = driver.find_element_by_css_selector("input[id='search_form_input_homepage']")
         search_input.send_keys("selenium", Keys.ENTER)
 
-        print('Taking screenshot...')
-        driver.get_screenshot_as_file(driver.session_id + '.png')
+        print("Taking screenshot...")
+        driver.get_screenshot_as_file(driver.session_id + ".png")
     finally:
         driver.quit()
