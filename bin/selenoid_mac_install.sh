641e4c04c6dc867777e602c4248752f55580234f
Igor Balagurov <balagurovi@yandex.ru>
2020-02-16 12:29:01 +0300

    Add selenoid ui

--- bin/selenoid_mac_install.sh
+++ bin/selenoid_mac_install.sh
@@ -2,5 +2,9 @@
 # owner @i.balagurov
 # the script for installation selenoid on MacOS
 
+echo "installation selenoid + 2 versions of opera / chrome / firefox"
 curl -s https://aerokube.com/cm/bash | bash \
 && ./cm selenoid start --vnc
+
+echo "installation selenoid ui"
+./cm selenoid-ui start
